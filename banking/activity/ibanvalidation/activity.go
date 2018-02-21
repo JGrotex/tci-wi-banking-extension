562157bcbe437ede404a33f1299a501ce7754089
Jörg Grote <joerg@grote-online.de>
2018-02-21 19:18:43 +0100

    fix

--- banking/activity/ibanvalidation/activity.go
+++ banking/activity/ibanvalidation/activity.go
@@ -1,11 +1,5 @@
 package ibanvalidation
 
-/*
- * Copyright © 2018. TIBCO Software Inc.
- * This file is subject to the license terms contained
- * in the license file that is distributed with this file.
- */
-
 import (
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
